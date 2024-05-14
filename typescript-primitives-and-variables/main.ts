119d3276d5bdcf44b9a41da4ceb86e3b3966f104
Jesus Rodriguez-Luna <JesusRodriguezLuna3616@gmail.com>
2024-05-14 21:58:43 +0000

    Completed typescript-primitives-and-variables

---
+++ typescript-primitives-and-variables/main.ts
@@ -0,0 +1,19 @@
+const fullName: string = 'Jesus Rodriguez-Luna';
+const isCool: boolean = true;
+const totalPets: number = 2;
+const empty: null = null;
+let nothing: undefined;
+
+// value of...
+console.log('value of fullName:', fullName);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets:', totalPets);
+console.log('value of empty:', empty);
+console.log('value of nothing', nothing);
+
+// typeof...
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+console.log('typeof empty:', typeof empty);
+console.log('typeof nothing:', typeof nothing);
