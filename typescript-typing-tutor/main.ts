b17f4e6240c1d4ecd1d2947ff233328679e38db2
Jesus Rodriguez-Luna <JesusRodriguezLuna3616@gmail.com>
2024-07-07 22:48:49 +0000

    Added Accuracy feature

--- typescript-typing-tutor/main.ts
+++ typescript-typing-tutor/main.ts
@@ -1,6 +1,8 @@
 const $listOfText = document.querySelectorAll('.text span');
 if (!$listOfText) throw new Error('$listOfText failed to query!');
 let currIndex = 0;
+let numOfIncorrect = 0;
+const sizeOfText = $listOfText.length;
 
 document.addEventListener('keydown', (event: KeyboardEvent) => {
   const key = event.key;
@@ -8,8 +10,24 @@ document.addEventListener('keydown', (event: KeyboardEvent) => {
   if (key === $listOfText[currIndex].textContent) {
     $listOfText[currIndex].className = 'correct';
     currIndex++;
-    $listOfText[currIndex].className = 'underline';
+    if (currIndex < sizeOfText) {
+      $listOfText[currIndex].className = 'underline';
+    }
   } else {
     $listOfText[currIndex].className = 'underline incorrect';
+    numOfIncorrect++;
+  }
+
+  // Accuracy
+  if (currIndex === sizeOfText) {
+    const $accuracyElem = document.createElement('h2');
+    $accuracyElem.setAttribute('class', 'accuracy');
+    const $parent = document.querySelector('div');
+
+    const totalKeystrokes = currIndex + numOfIncorrect;
+    const accuracy = (currIndex / totalKeystrokes) * 100;
+
+    $accuracyElem.textContent = `Accuracy: ${accuracy.toFixed(2)}%`;
+    $parent?.appendChild($accuracyElem);
   }
 });
