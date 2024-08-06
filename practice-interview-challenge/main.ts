afa15f177bea9c98f4114941d0acf89288548987
Jesus Rodriguez-Luna <JesusRodriguezLuna3616@gmail.com>
2024-08-06 02:12:20 +0000

    Completed practice-interview-problem

---
+++ practice-interview-challenge/main.ts
@@ -0,0 +1,34 @@
+function getRangeReport(start: number, end: number): object {
+  let total = 0; // the sum of all integers from start to end.
+  const odds = []; // an array containing all of the odd integers from start to end.
+  const evens = []; // an array containing all of the even integers from start to end.
+  const range = []; // an array containing all integers from start to end.
+  let avg = 0; // the average of all integers from start to end.
+
+  for (let i = start; i <= end; i++) {
+    total += i; // total
+
+    // if even, else odd
+    if (i % 2 === 0) {
+      evens.push(i);
+    } else {
+      odds.push(i);
+    }
+
+    // range
+    range.push(i);
+  }
+
+  // average
+  avg = total / range.length;
+
+  const obj = {
+    average: avg,
+    evens: evens,
+    odds: odds,
+    range: range,
+    total: total,
+  };
+
+  return obj;
+}
