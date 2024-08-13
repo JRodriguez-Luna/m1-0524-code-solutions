fb2aa9a5ca51adf046d31fbd378905ced6018b61
Jesus Rodriguez-Luna <JesusRodriguezLuna3616@gmail.com>
2024-08-13 01:33:30 +0000

    Completed array-methods

---
+++ array-methods/find.js
@@ -0,0 +1,17 @@
+"use strict";
+const users = [
+    { id: 1, name: 'John' },
+    { id: 2, name: 'Jane' },
+    { id: 3, name: 'Bob' },
+];
+const products = [
+    { id: 1, name: 'iPhone', price: 999 },
+    { id: 2, name: 'Samsung Galaxy', price: 899 },
+    { id: 3, name: 'Google Pixel', price: 799 },
+];
+// find the first object inside of the users array
+const user = users.find(({ id }) => id === 2);
+console.log('user:', user);
+// find the first object inside of the products array
+const expensiveProduct = products.find(({ price }) => price > 900);
+console.log('expensiveProduct:', expensiveProduct);
---
+++ array-methods/map.ts
@@ -0,0 +1,12 @@
+const prices = [42.42, 10, 28.2234, 3.2, 5, 12];
+
+// create new array containing objects
+const price = prices.map((org_num) => ({
+  price: org_num,
+  salePrice: org_num / 2,
+}));
+console.log('price:', price);
+
+// create new array containing number format
+const formatPrice = prices.map((num) => `$${num}`);
+console.log('formatPrice:', formatPrice);
